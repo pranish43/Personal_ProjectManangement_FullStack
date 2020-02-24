2be6e7b2e257908b6aa25565b4e1c33c9bcf5560
pranish43 <pranishkarki43@gmail.com>
2020-02-23 20:16:51 -0500

    firstCommit

---
+++ PersonalProjectManagement/src/main/java/com/PersonalProjectManagement/PersonalProjectManagement/PersonalProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.PersonalProjectManagement.PersonalProjectManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectManagementApplication.class, args);
+	}
+
+}
