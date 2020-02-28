fa74bd949cc63adfefb1c22292d316a0065d3336
pranish43 <pranishkarki43@gmail.com>
2020-02-28 00:01:05 -0500

    Added Validation

--- PersonalProjectManagement/src/main/java/com/PersonalProjectManagement/PersonalProjectManagement/services/ProjectService.java
+++ PersonalProjectManagement/src/main/java/com/PersonalProjectManagement/PersonalProjectManagement/services/ProjectService.java
@@ -1,4 +1,19 @@
 package com.PersonalProjectManagement.PersonalProjectManagement.services;
 
+import com.PersonalProjectManagement.PersonalProjectManagement.domain.Project;
+import com.PersonalProjectManagement.PersonalProjectManagement.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+
+
+        return projectRepository.save(project);
+    }
 }
