6d8a832ca21ce175cb83ede3fbb349a468ba48a2
pranish43 <pranishkarki43@gmail.com>
2020-02-28 18:05:30 -0500

    Custom Validation added

--- PersonalProjectManagement/src/main/java/com/PersonalProjectManagement/PersonalProjectManagement/services/ProjectService.java
+++ PersonalProjectManagement/src/main/java/com/PersonalProjectManagement/PersonalProjectManagement/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.PersonalProjectManagement.PersonalProjectManagement.services;
 
 import com.PersonalProjectManagement.PersonalProjectManagement.domain.Project;
+import com.PersonalProjectManagement.PersonalProjectManagement.exceptions.ProjectIdException;
 import com.PersonalProjectManagement.PersonalProjectManagement.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,16 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+
+
     public Project saveOrUpdateProject(Project project){
 
-
+    try{
+        project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
         return projectRepository.save(project);
+
+    }catch (Exception e){
+        throw new ProjectIdException("Project Id :"+ project.getProjectIdentifier().toUpperCase()+" already exists" );
+    }
     }
 }
--- PersonalProjectManagement/src/main/java/com/PersonalProjectManagement/PersonalProjectManagement/web/ProjectController.java
+++ PersonalProjectManagement/src/main/java/com/PersonalProjectManagement/PersonalProjectManagement/web/ProjectController.java
@@ -2,13 +2,21 @@ package com.PersonalProjectManagement.PersonalProjectManagement.web;
 
 import com.PersonalProjectManagement.PersonalProjectManagement.domain.Project;
 import com.PersonalProjectManagement.PersonalProjectManagement.services.ProjectService;
+import com.PersonalProjectManagement.PersonalProjectManagement.services.ValidationMapErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -17,9 +25,16 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
-    @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    @Autowired
+    private ValidationMapErrorService validationMapErrorService;
 
+    @PostMapping("")
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        ResponseEntity<?> errorMap= validationMapErrorService.ValidationMapService(result);
+    if(errorMap!=null){
+        return errorMap;
+    }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
