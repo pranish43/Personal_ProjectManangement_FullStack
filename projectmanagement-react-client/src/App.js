708643ad981d9b4e7a142e3dfd7e8904c2ac2655
pranish43 <pranishkarki43@gmail.com>
2020-03-01 12:03:29 -0600

    frontend

--- projectmanagement-react-client/src/App.js
+++ projectmanagement-react-client/src/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import "bootstrap/dist/css/bootstrap.min.css"; 
+import {BrowserRouter as Router, Route } from "react-router-dom"
+import AddProject from './components/Project/AddProject';
+import Header from './components/Layout/Header';
 
 class App extends Component {
   render(){
   return (
+    <Router>
     <div className="App">
-    <Dashboard/>
-    </div>)
+  <Header/>
+    
+    <Route  exact path="/dashboard" component={Dashboard}/>
+    <Route exact path="/addProject" component={AddProject} />
+    </div>
+    </Router>
+  )
   }
 }
 
