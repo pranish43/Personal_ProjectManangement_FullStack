1d2b24020f73f8bba95fe21920233cd6ffdeddd5
pranish43 <pranishkarki43@gmail.com>
2020-03-01 21:19:27 -0600

    redux

--- projectmanagement-react-client/src/App.js
+++ projectmanagement-react-client/src/App.js
@@ -5,10 +5,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router, Route } from "react-router-dom"
 import AddProject from './components/Project/AddProject';
 import Header from './components/Layout/Header';
+import {Provider} from "react-redux";
+import store from './store';
 
 class App extends Component {
   render(){
   return (
+   <Provider store={store}>
     <Router>
     <div className="App">
   <Header/>
@@ -17,6 +20,7 @@ class App extends Component {
     <Route exact path="/addProject" component={AddProject} />
     </div>
     </Router>
+    </Provider>
   )
   }
 }
