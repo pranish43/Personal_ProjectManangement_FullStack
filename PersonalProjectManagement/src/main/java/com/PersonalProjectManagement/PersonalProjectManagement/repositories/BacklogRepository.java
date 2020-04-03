6dc16a9b1283049ef6e32669c2b999da8dd08d22
pranish43 <pranishkarki43@gmail.com>
2020-04-03 15:16:06 -0500

    Backlog added

---
+++ PersonalProjectManagement/src/main/java/com/PersonalProjectManagement/PersonalProjectManagement/repositories/BacklogRepository.java
@@ -0,0 +1,9 @@
+package com.PersonalProjectManagement.PersonalProjectManagement.repositories;
+
+import com.PersonalProjectManagement.PersonalProjectManagement.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog,Long> {
+}
