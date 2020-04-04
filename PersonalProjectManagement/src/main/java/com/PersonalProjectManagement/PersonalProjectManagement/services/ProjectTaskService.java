d59491a2f6f3e05a5332e1d75b85f9ab36ba8e06
pranish43 <pranishkarki43@gmail.com>
2020-04-03 19:48:07 -0500

    Persist project tast

---
+++ PersonalProjectManagement/src/main/java/com/PersonalProjectManagement/PersonalProjectManagement/services/ProjectTaskService.java
@@ -0,0 +1,38 @@
+package com.PersonalProjectManagement.PersonalProjectManagement.services;
+
+import com.PersonalProjectManagement.PersonalProjectManagement.domain.Backlog;
+import com.PersonalProjectManagement.PersonalProjectManagement.domain.Project;
+import com.PersonalProjectManagement.PersonalProjectManagement.domain.ProjectTask;
+import com.PersonalProjectManagement.PersonalProjectManagement.repositories.BacklogRepository;
+import com.PersonalProjectManagement.PersonalProjectManagement.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService  {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        projectTask.setBacklog(backlog);
+        Integer BacklogSequence  = backlog.getPTSequence();
+        BacklogSequence++;
+
+        projectTask.setProjectSequence(projectIdentifier +"-"+ BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        if(projectTask.getPriority()==0||projectTask.getPriority()==null){
+            projectTask.setPriority(3);
+        }
+        if(projectTask.getStatus()==""|| projectTask.getStatus()==null){
+            projectTask.setStatus("TO_DO");
+        }
+        return projectTaskRepository.save(projectTask);
+
+    }
+}
---
+++ PersonalProjectManagement/src/main/java/com/PersonalProjectManagement/PersonalProjectManagement/web/BacklogController.java
@@ -0,0 +1,37 @@
+package com.PersonalProjectManagement.PersonalProjectManagement.web;
+
+import com.PersonalProjectManagement.PersonalProjectManagement.domain.ProjectTask;
+import com.PersonalProjectManagement.PersonalProjectManagement.services.ProjectTaskService;
+import com.PersonalProjectManagement.PersonalProjectManagement.services.ValidationMapErrorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/backing")
+@CrossOrigin
+public class BacklogController {
+
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    private ValidationMapErrorService validationMapErrorService;
+
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask, BindingResult result, @PathVariable String backlog_id){
+        ResponseEntity<?> errorMap= validationMapErrorService.ValidationMapService(result);
+        if(errorMap != null) return errorMap;
+
+        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id,projectTask);
+
+        return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+
+
+    }
+}
+
